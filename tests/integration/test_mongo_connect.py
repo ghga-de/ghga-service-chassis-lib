8aa7f1e53e3817101816f64c8ff2c462d333d17b
Moritz Hahn <Moritz.Hahn@uni-tuebingen.de>
2021-10-06 12:01:42 +0000

    Removed fixture

--- tests/integration/test_mongo_connect.py
+++ tests/integration/test_mongo_connect.py
@@ -12,3 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from ghga_service_chassis_lib.mongo_connect import DBConnect
+
+# Here should be a test for the mongo_connect functionality
